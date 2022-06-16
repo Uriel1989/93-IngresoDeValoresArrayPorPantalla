94aaef413c111b1f61bb52e0dad385e61083198b
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:59:51 -0300

    Add files via upload

---
+++ 93-IngresoDeValoresArrayPorPantalla/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+//Ingreso de valores por pantalla y lo muestra
+
+namespace _93_IngresoDeValoresArrayPorPantalla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa 3 valores para el array propuesto:");
+
+            int[] array = new int[3];
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            array[0] = a;
+            array[1] = b;
+            array[2] = c;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("El valor de la posicion " + i + " es " + array[i]);
+            }
+        }
+    }
+}
